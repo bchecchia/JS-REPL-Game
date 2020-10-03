26143a0a94e8f77c2b7ec4e1355b84e3f735e941
bchecchia <checchia@msn.com>
2020-10-02 21:12:02 -0400

    update

--- index.js
+++ index.js
@@ -18,3 +18,31 @@ switch(option) {
     console.log( "Prisoner: \"Quit messin' around.\"" );
 } 
 
+switch(x) {
+  case 'value1':  // if (x === 'value1')
+    ...
+    [break]
+
+  case 'value2':  // if (x === 'value2')
+    ...
+    [break]
+
+  default:
+    ...
+    [break]
+}
+
+
+switch(x) {
+  case 'value1':  // if (x === 'value1')
+    ...
+    [break]
+
+  case 'value2':  // if (x === 'value2')
+    ...
+    [break]
+
+  default:
+    ...
+    [break]
+}
